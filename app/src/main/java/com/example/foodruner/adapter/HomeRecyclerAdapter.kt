d86e2ff0c337a8c8763ddef1c4c7721b59d5c09f
Sheeban10 <sheebanfareedi.sf@gmail.com>
2023-09-18 14:53:49 +0530

    recycler layout

--- app/src/main/java/com/example/foodruner/adapter/HomeRecyclerAdapter.kt
+++ app/src/main/java/com/example/foodruner/adapter/HomeRecyclerAdapter.kt
@@ -1,27 +1,49 @@
 package com.example.foodruner.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.foodruner.R
 import com.example.foodruner.model.Restaurants
+import com.squareup.picasso.Picasso
 
 class HomeRecyclerAdapter(val context : Context, val itemList : ArrayList<Restaurants>) : RecyclerView.Adapter<HomeRecyclerAdapter.HomeViewHolder>() {
 
-    class HomeViewHolder(view: View): RecyclerView.ViewHolder(view){
 
-    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeViewHolder {
-        TODO("Not yet implemented")
+
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.restaurants_recycler_view, parent, false)
+            return HomeViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return itemList.size
     }
 
     override fun onBindViewHolder(holder: HomeViewHolder, position: Int) {
-        TODO("Not yet implemented")
+
+        val restaurants = itemList[position]
+
+        holder.tvRestName.text = restaurants.restName
+        holder.tvCost.text = restaurants.restCost
+        holder.tvRating.text = restaurants.restRating
+        Picasso.get().load(restaurants.restImage).error(R.drawable.rest_image_def).into(holder.imgRest)
     }
 
+
+    class HomeViewHolder(view: View): RecyclerView.ViewHolder(view){
+
+        val tvRestName : TextView = view.findViewById(R.id.tvNameRestaurant)
+        val tvCost : TextView = view.findViewById(R.id.tvCost)
+        val tvRating : TextView = view.findViewById(R.id.tvRating)
+        val imgRest : ImageView = view.findViewById(R.id.imgRestaurant)
+        val btnFav : Button = view.findViewById(R.id.btnFavourites)
+
+    }
 }
--- app/src/main/java/com/example/foodruner/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/foodruner/fragments/HomeFragment.kt
@@ -5,15 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.RelativeLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.foodruner.R
+import com.example.foodruner.adapter.HomeRecyclerAdapter
+import com.example.foodruner.model.Restaurants
 
 class HomeFragment : Fragment() {
 
+    lateinit var recyclerHome : RecyclerView
+    lateinit var layoutManager : RecyclerView.LayoutManager
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    lateinit var recyclerAdapter : HomeRecyclerAdapter
+    lateinit var progressLayout : RelativeLayout
+    lateinit var progressBar : ProgressBar
 
-    }
+    var restaurantsList = arrayListOf<Restaurants>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,6 +30,18 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+
+
+        progressLayout = view.findViewById(R.id.rlHome)
+        progressBar = view.findViewById(R.id.progressBar)
+
+        progressLayout.visibility = View.VISIBLE
+
+        recyclerHome = view.findViewById(R.id.recyclerHome)
+
+        layoutManager = LinearLayoutManager(activity)
+
+
     }
 
 
